d2e6a3064dbcb845e49630709057f99ab1a9fe87
Akhilsunny212 <78971481+Akhilsunny212@users.noreply.github.com>
2025-02-04 16:06:39 +0530

    Update prompt.py

--- src/prompt.py
+++ src/prompt.py
@@ -3,6 +3,5 @@ prompt_template="""
     if you are not aware of it, just say you are not aware of it, don't try to make up an answer.
     Context:{context}
     Question:{question}
-    
     only return the helpful answer.
     Helpful answer:"""
